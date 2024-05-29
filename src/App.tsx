8092a9afdcd1e934b12b3afbe8b0081b44b44e91
Igor Stepanov <stipyk1309@gmail.com>
2024-05-29 19:40:23 +0300

    feat: add footer with github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { EventList, VideoPlayer } from './components'
+import { EventList, VideoPlayer, Footer } from './components'
 import styles from './App.module.css'
 
 function App() {
 	return (
-		<div className={styles.container}>
-			<VideoPlayer />
-			<EventList />
-		</div>
+		<>
+			<div className={styles.container}>
+				<VideoPlayer />
+				<EventList />
+			</div>
+			<Footer />
+		</>
 	)
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './event-list'
 export * from './video-player'
 export * from './event-rect'
+export * from './footer'
