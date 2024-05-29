6cd6aac6555bd40cf6da4d535b9930dfc2a2c976
Igor Stepanov <stipyk1309@gmail.com>
2024-05-30 00:52:33 +0300

    refactor: VideoPlayer delete unused code

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import styles from './App.module.css'
 function App() {
 	return (
 		<>
-			<div className={styles.container}>
+			<main className={styles.container}>
 				<VideoPlayer />
 				<EventList />
-			</div>
+			</main>
 			<Footer />
 		</>
 	)
--- src/components/video-player/VideoPlayer.tsx
+++ src/components/video-player/VideoPlayer.tsx
@@ -3,11 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { RootState, Event } from '../../types'
 import { fetchEvents } from '../../store/actions'
 import { EventRect } from '../event-rect'
-import styles from './VideoPlayer.module.css'
 
 function VideoPlayer() {
 	const videoRef = useRef<HTMLVideoElement>(null)
-	const containerRef = useRef<HTMLDivElement>(null)
 	const dispatch = useDispatch()
 	const events = useSelector((state: RootState) => state.events)
 
@@ -30,7 +28,7 @@ function VideoPlayer() {
 	}
 
 	return (
-		<div ref={containerRef} className={styles.container}>
+		<>
 			<video ref={videoRef} controls onTimeUpdate={handleTimeUpdate}>
 				<source
 					src='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
@@ -40,7 +38,7 @@ function VideoPlayer() {
 			{activeEvents.map((event: Event) => (
 				<EventRect key={event.timestamp} zone={event.zone} />
 			))}
-		</div>
+		</>
 	)
 }
 
